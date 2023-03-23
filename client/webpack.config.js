fec2c582c3113beeab1cd10266d392008417a37d
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-03-23 18:38:01 -0500

    bcs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,32 +24,32 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new MiniCssExtractPlugin(),
-      // new InjectManifest({
-      //   swSrc: './src/sw.js',
-      //   swDest: 'service-worker.js',
-      // }), 
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }), 
       new WebpackPwaManifest({
         name: 'Justins Text Editor',
         short_name: 'JATE',
         description: 'Just Another Text Editor!',
         background_color: '#ffffff',
         // crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
-        // icons: [
-        //   {
-        //     src: path.resolve('src/assets/icon.png'),
-        //     sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-        //   },
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
     
-        //   {
-        //     src: path.resolve('src/assets/large-icon.png'),
-        //     size: '1024x1024' // you can also use the specifications pattern
-        //   },
-        //   {
-        //     src: path.resolve('src/assets/maskable-icon.png'),
-        //     size: '1024x1024',
-        //     purpose: 'maskable'
-        //   }
-        // ]
+          // {
+          //   src: path.resolve('src/assets/large-icon.png'),
+          //   size: '1024x1024' // you can also use the specifications pattern
+          // },
+          // {
+          //   src: path.resolve('src/assets/maskable-icon.png'),
+          //   size: '1024x1024',
+          //   purpose: 'maskable'
+          // }
+        ]
       })
       
     ],
