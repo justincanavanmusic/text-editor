43ffe83c49684c98bac3589174af8cb005d49f47
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-03-23 10:05:55 -0500

    npm run start and build working, have to fix injectManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
@@ -18,12 +19,61 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      // new InjectManifest({
+      //   swSrc: './src/sw.js',
+      //   swDest: 'service-worker.js',
+      // }), 
+      new WebpackPwaManifest({
+        name: 'Justins Text Editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor!',
+        background_color: '#ffffff',
+        // crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        // icons: [
+        //   {
+        //     src: path.resolve('src/assets/icon.png'),
+        //     sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+        //   },
+    
+        //   {
+        //     src: path.resolve('src/assets/large-icon.png'),
+        //     size: '1024x1024' // you can also use the specifications pattern
+        //   },
+        //   {
+        //     src: path.resolve('src/assets/maskable-icon.png'),
+        //     size: '1024x1024',
+        //     purpose: 'maskable'
+        //   }
+        // ]
+      })
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
